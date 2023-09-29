6fc8bb356fa3fc1e184fd23faa8566738aa4bbe5
Rafael Vieira <devrafaelvieira@gmail.com>
2023-09-28 21:16:26 -0300

    melhorando estrutura do blog routes.py
    Adicionando as rotas para arquivo de rotas, deixando o app.py sendo nosso init

---
+++ api/routes.py
@@ -0,0 +1,23 @@
+# routes.py
+from app import app, flatpages
+
+POST_DIR = 'posts'
+
+@app.route("/")
+def index():
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    return render_template('posts.html', posts=posts)
+
+@app.route('/posts/<name>/')
+def post(name):
+    path = '{}/{}'.format(POST_DIR, name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
+
+@app.route('/sobre/')
+def sobre():
+    return post('sobre')
+
+@app.route('/contato')
+def contato():
+    return post('contato')
