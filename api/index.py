3252c6a3e43b5d8776576db8b4ee7b02dcbae43f
Rafael Vieira <devrafaelvieira@gmail.com>
2023-09-28 21:17:09 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,41 +1,16 @@
-import os
-import sys
-from flask import Flask, render_template
-from flask_flatpages import FlatPages, pygments_style_defs
+# app.py
+from flask import Flask
+from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
-DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
-FLATPAGES_ROOT = 'content'
-POST_DIR = 'posts'
-
 app = Flask(__name__)
-flatpages = FlatPages(app)
-freezer = Freezer(app)
 app.config.from_object(__name__)
 
-@app.route("/")
-def index():
-    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-    return render_template('posts.html', posts=posts)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
 
-@app.route('/posts/<name>/')
-def post(name):
-    path = '{}/{}'.format(POST_DIR, name)
-    post = flatpages.get_or_404(path)
-    return render_template('post.html', post=post)
-
-@app.route('/sobre/')
-def sobre():
-    return post('sobre')
-
-@app.route('/contato')
-def contato():
-    return post('contato')
+# Import routes
+from routes import *
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        freezer.freeze()
-    else:
-        app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
